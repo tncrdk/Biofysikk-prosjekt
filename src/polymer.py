f8287504bb7df9a8c48b1e7ad15ae37fbfffc9ed
Vemund Aakre <vemuna@stud.ntnu.no>
2024-02-01 12:49:35 +0100

    Lagt til checkintact_3

--- src/polymer.py
+++ src/polymer.py
@@ -6,6 +6,7 @@ b = np.array([[-3, -5], [-2, -1], [-1, 3]])
 c = np.array([[3, -4], [-1, 0], [-5, -4]])
 d = np.array([[1, 0], [1, 1], [1, 2], [1, 1]])
 
+test_polymer = np.random.randint(0, 35, (35,2))
 
 def check_if_intact(polymer: np.ndarray, polymer_length: int) -> bool:
     """Sjekker om en polymer er intakt
@@ -17,8 +18,8 @@ def check_if_intact(polymer: np.ndarray, polymer_length: int) -> bool:
     Returns:
         bool: True hvis polymeren er intakt
     """
-    if np.size(np.unique(polymer, axis = 0), axis = 0) != polymer_length:
-        return False 
+    if np.size(np.unique(polymer, axis=0), axis=0) != polymer_length:
+        return False
     # Sjekker både at den har N monomerer og da har de alle en unik heltallsrepresentasjon
     for i in range(1, polymer_length):
         distance = (polymer[i - 1, 0] - polymer[i, 0]) ** 2 + (
@@ -39,8 +40,8 @@ def check_if_intact_2(polymer: np.ndarray, polymer_length: int) -> bool:
     Returns:
         bool: True hvis polymeren er intakt
     """
-    if np.size(np.unique(polymer, axis = 0), axis = 0) != polymer_length:
-        return False 
+    if np.size(np.unique(polymer, axis=0), axis=0) != polymer_length:
+        return False
         # Sjekker både at den har N monomerer og da har de alle en unik heltallsrepresentasjon
 
     test = polymer[1:]
@@ -54,4 +55,45 @@ def check_if_intact_2(polymer: np.ndarray, polymer_length: int) -> bool:
         return False
     return True
 
-print(check_if_intact(d, 4))
+
+def check_if_intact_3(polymer: np.ndarray, polymer_length: int) -> bool:
+    """Sjekker om en polymer er intakt
+
+    Args:
+        polymer (np.ndarray): polymeren som sjekkes
+        polymer_length (int): lengden til polymeren
+
+    Returns:
+        bool: True hvis polymeren er intakt
+    """
+    if np.size(np.unique(polymer, axis=0), axis=0) != polymer_length:
+        return False
+        # Sjekker både at den har N monomerer og da har de alle en unik heltallsrepresentasjon
+
+    test = polymer[1:]
+    test_mot = polymer[:-1]
+    distance_array = np.abs(test[:, 0] - test_mot[:, 0]) + np.abs(
+        test[:, 1] - test_mot[:, 1]
+    )  ## Trenger ikke kvadratrot fordi alt annet enn 1 som verdi er ikke intakt(også raskere)
+    if np.any(
+        distance_array != 1
+    ):  ## Hvis distance_array inneholder noe annet enn 1 er ikke polymer intakt; Kan bli raskere???
+        return False
+    return True
+
+
+time_start_1 = time.perf_counter()
+check_if_intact(test_polymer, 35)
+time_end_1 = time.perf_counter() - time_start_1
+
+time_start_2 = time.perf_counter()
+check_if_intact_2(test_polymer, 35)
+time_end_2 = time.perf_counter() - time_start_2
+
+time_start_3 = time.perf_counter()
+check_if_intact_3(test_polymer, 35)
+time_end_3 = time.perf_counter() - time_start_3
+
+print(time_end_1)
+print(time_end_2)
+print(time_end_3)
